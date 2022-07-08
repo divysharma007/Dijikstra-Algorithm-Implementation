f24533f34fa66870d274ede6b4471b7b4624efa4
Divya Sharma <t-disharma@microsoft.com>
2022-07-08 18:42:11 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Introduction
+Program to find the shortest path from a given source node to other nodes. \
+Used Dijikstra algorithm to get the output. The output is optimal in terms of memory and space requirements. \
+# Complexity
+Time Complexity - O((n+e)log(n)) \
+Space Complexity - O(n) \
+where n is the number of nodes and e is the number of edges
+
+# Assumptions
+ - Directed acylic Graph (DAG)
+ - All edge values are positive
+ - Maximum distance of a node from the source is less than 2^31-1(INT sotrage capacity)
+ - n is less than (2^31-1)
+ - nodes are in the form of 0,1 ... n-1
+
+where n is the number of nodes and e is the number of edges 
+
+# Usage
+```
+$ g++ assignment.cpp -o assignment
+$ assignment
+```
---
+++ assignment.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * @brief Takes input and forms the adjacency list
+ * @param numberOfNodes number of nodes in the graph
+ * @param numberOfEdges number of total edges in the graph
+ * @return Adjacency list containing neightbours of each node
+ */
+vector<vector<pair<int,int>>> getNeighbours(int numberOfNodes,int numberOfEdges){
+  vector<vector<pair<int,int>>>neighbours(numberOfNodes);
+
+   for(int i=0;i<numberOfEdges;i++){
+       int parentNode,childNode,edgeValue;
+       cin>>parentNode>>childNode>>edgeValue;
+       neighbours[parentNode].push_back({edgeValue, childNode});
+   }
+   return neighbours;
+}
+
+/**
+ * @brief Uses dijkstra algorithm and finds the shortest distance of all the nodes from the source node
+ * @param sourceNode Node from which distances are to be found
+ * @param neighbours List contaning neighbours to which the
+ * @return returns the shortest distance of all nodes from the source node
+ */
+vector<int> findShortestPath(int sourceNode,vector<vector<pair<int,int>>> neighbours){
+int numberOfNodes=neighbours.size();
+ vector<int>distanceVector(numberOfNodes,INT_MAX);
+ priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+   pq.push({0,sourceNode});
+   while(!pq.empty()){
+       int node=pq.top().second,
+       distance=pq.top().first;
+       pq.pop();
+       distanceVector[node]=min(distanceVector[node],distance);
+       for(int child=0;child<neighbours[node].size();child++){
+           pq.push({distance+neighbours[node][child].first, neighbours[node][child].second});
+       }
+   }
+   return distanceVector;
+}
+/**
+ * @brief Prints the shortest distance of node from the source node
+ * @param distanceVector Vector storing shortest distance 
+ */
+void printOutput(vector<int>distanceVector){
+int numberOfNodes=distanceVector.size();
+   for(int node=0;node<numberOfNodes;node++)
+       cout << "Distance of node " <<node <<" from the source node is " <<distanceVector[node] << endl;
+   
+}
+int main()
+{
+    int numberOfNodes,numberOfEdges,sourceNode;
+    cin>>numberOfNodes>>numberOfEdges>>sourceNode;
+    vector<vector<pair<int,int>>> neighbours=getNeighbours(numberOfNodes,numberOfEdges);
+    vector<int>distanceVector=findShortestPath(sourceNode,neighbours);
+    printOutput(distanceVector);
+
+    return 0;
+}
